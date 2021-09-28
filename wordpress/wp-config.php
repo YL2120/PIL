dea336159ade64577fccf17e41d3e13ae6f9c10e
AudricSkivée <audricskivee@gmail.com>
2021-09-28 16:59:56 +0200

    first html pieces

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -64,6 +64,9 @@ define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', getenv_docker('WORDPRESS_DB_COLLATE', '') );
 
+/* Make WordPress install plugins directly */
+define('FS_METHOD', 'direct');
+
 /**#@+
  * Authentication unique keys and salts.
  *
