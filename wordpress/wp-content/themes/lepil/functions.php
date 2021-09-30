8613394dda11fd903685ae1f9a5c196c6bfe3d94
Audric Skivée <audricskivee@gmail.com>
2021-09-30 11:20:09 +0200

    test svg

--- wordpress/wp-content/themes/lepil/functions.php
+++ wordpress/wp-content/themes/lepil/functions.php
@@ -21,3 +21,12 @@ function WPTheme_register_scripts(){
 }
 
 add_action('wp_enqueue_scripts','WPTheme_register_scripts'); // When wp_enqueue, use the function;
+
+function add_file_types_to_uploads($file_types)
+{
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes);
+    return $file_types;
+}
+add_filter('upload_mimes', 'add_file_types_to_uploads');
